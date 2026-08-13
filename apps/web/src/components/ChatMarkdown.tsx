98a7dbab9c76f94eb091134c37d648ceb38dcfa7
Mateus Bento <mateusbentoa@gmail.com>
2026-08-13 09:40:20 -0300

    # This is a combination of 2 commits. # This is the 1st commit message:
    feat: add mermaid to chat dialog

--- apps/web/src/components/ChatMarkdown.tsx
+++ apps/web/src/components/ChatMarkdown.tsx
@@ -190,6 +190,7 @@ import {
 } from "../browser/openFileInPreview";
 import { resolveLinkTarget } from "../browser/browserLinkTarget";
 import { PullRequestLinkPreview } from "./pullRequest/PullRequestLinkPreview";
+import { MermaidDiagram } from "./MermaidDiagram";
 
 interface ChatMarkdownProps {
   text: string;
@@ -3149,7 +3150,7 @@ const CHAT_MARKDOWN_COMPONENTS = {
 
     const language = extractFenceLanguage(codeBlock.className);
     const fenceTitle = extractFenceTitle(extractPreCodeMeta(node));
-    return (
+    const codeFallback = (
       <MarkdownCodeBlock
         code={codeBlock.code}
         language={language}
@@ -3171,6 +3172,17 @@ const CHAT_MARKDOWN_COMPONENTS = {
         </RenderErrorBoundary>
       </MarkdownCodeBlock>
     );
+    if (!isStreaming && language.toLowerCase() === "mermaid") {
+      return (
+        <MermaidDiagram
+          code={codeBlock.code}
+          theme={resolvedTheme}
+          fenceTitle={fenceTitle}
+          fallback={codeFallback}
+        />
+      );
+    }
+    return codeFallback;
   },
 } satisfies Components;
 
